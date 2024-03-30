23d04a8b7cf256989c590d0284cd377cac61a684
azesm <azesm@yahoo.com>
2024-03-30 20:31:49 +0300

    3_01.47.48_laba_5_table?

---
+++ inc/table.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+# Лабораторная 5
+/*
+//$cols = 10;
+//$rows = 10;
+//$color = '#008080';
+
+function draw_table($cols = 10, $rows = 10, $color = "#FFDB00") {
+    echo "<table border='1'>";
+    for ($tr = 1; $tr <= $rows; $tr++) {
+        echo "<tr>";
+        for ($td = 1; $td <= $cols; $td++) {
+            If($td == 1 || $tr == 1)
+                echo "<th bgcolor=$color>" . $td * $tr . "</th>";
+            else
+                echo "<td>" . $td * $tr . "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+};
+*/
+
